029132e4b8f981c55cff83259e0c9c94c971bf79
Jakob Edelstein <jakobedelstein@gmail.com>
2024-01-07 05:46:55 -0500

    Post

---
+++ src/components/posts/NoSleep.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<p>
+			This has not been a great week for sleep. Although I usually
+			wake up throughout the night, recently I've been up for 
+			hours at a time. Waking up at 2am and staying up until
+			morning. As far as I can tell, nothing in my routine
+			has changed, and I feel fine otherwise.
+		</p>
+		<p>
+			I have to acknowledge the two year cycle of a certain pattern.
+			In January 2020 and 2022, I experience a certain kind of shock.
+			I'm not ready to write about it just yet. But the pattern of 
+			behavior begain with a series of sleepless nights. I have tools
+			to keep this spiraling out of control, but I can't deny that
+			it's on my mind.
+		</p>
+		<p>
+			I think it's important that I share my concerns with Melissa. I'm
+			not sure she'll totally understand, but I know she will be supportive.
+			Don't feel like I have much more to say at the moment. Time to go
+			to the gym, and get ready for Xel-Ha
+		</p>
+	</div>
+</template>
