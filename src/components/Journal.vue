22aab42642b7a82bb3de059179b441c9c14dca05
Jakob Edelstein <jakobedelstein@gmail.com>
2024-01-07 05:47:25 -0500

    config for new post

--- src/components/Journal.vue
+++ src/components/Journal.vue
@@ -6,6 +6,11 @@ const postConfig = [
 		postName: 'WhatAreWeDoingHere',
 		title: 'What are we doing here?',
 		timestamp: '5:15am | January 5th, 2024'
+	},
+	{
+		postName: 'NoSleep',
+		title: 'No Sleep',
+		timestamp: '5:30 am | January 7th, 2024'
 	}
 ]
 
