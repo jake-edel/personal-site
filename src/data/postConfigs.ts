5704316cb941db8e929725d8be892d505e5c1480
Jakob Edelstein <jakobedelstein@gmail.com>
2024-01-28 21:51:33 -0500

    post configs into separate file

---
+++ src/data/postConfigs.ts
@@ -0,0 +1,21 @@
+const postConfig = [
+	{
+		postName: 'WhatAreWeDoingHere',
+		title: 'What are we doing here?',
+		timestamp: '5:15am | January 5th, 2024'
+	},
+	{
+		postName: 'NoSleep',
+		title: 'No Sleep',
+		timestamp: '5:30 am | January 7th, 2024'
+	},
+	{
+		postName: 'ReadingList2023',
+		title: '2023 Reading List',
+		timestamp: '9:02 pm | January 28th, 2024'
+	}
+]
+
+declare module 'postConfigs' {
+	export default postConfig
+}
