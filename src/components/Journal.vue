eba4bf04fb992e62ef3a9771e239b6a972106510
Jakob Edelstein <jakobedelstein@gmail.com>
2024-02-09 05:24:57 -0500

    WIP: getting module export/import to work

--- src/components/Journal.vue
+++ src/components/Journal.vue
@@ -1,24 +1,14 @@
 <script setup lang="ts">
 import JournalPost from './JournalPost.vue'
-
-const postConfig = [
-	{
-		postName: 'WhatAreWeDoingHere',
-		title: 'What are we doing here?',
-		timestamp: '5:15am | January 5th, 2024'
-	},
-	{
-		postName: 'NoSleep',
-		title: 'No Sleep',
-		timestamp: '5:30 am | January 7th, 2024'
-	}
-]
-
+import { postConfigs } from '@/data/postConfigs.js'
+import { test } from '@/data/test'
 </script>
 
 <template>
-	<div v-for="post in postConfig">
-		<JournalPost :postConfig="post" :postName="'WhatAreWeDoingHere'"/>
+	{{  postConfigs }}
+	{{  test }}
+	<div v-for="post in postConfigs">
+		<JournalPost :postConfig="post"/>
 	</div>
 </template>
 
