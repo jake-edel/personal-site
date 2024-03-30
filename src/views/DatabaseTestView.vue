ee3e7c40df84d4427eb5743c0e3b6c652a39da3d
Jakob Edelstein <jakobedelstein@gmail.com>
2024-03-29 21:56:25 -0500

    basic controls to retrieve, add, and delete from DB

--- src/views/DatabaseTestView.vue
+++ src/views/DatabaseTestView.vue
@@ -1,25 +1,69 @@
 <script setup lang="ts">
-async function fetchTableThen() {
-	const response = fetch('http://localhost:3001/')
-	// const response = fetch('http://127.0.0.1:3001/') <= how to get this to work?
-		.then(response => response.json())
-		.then(json => json)
-	console.log(await response)
+import { ref } from 'vue';
+
+// async function fetchTableThen() {
+// 	const response = fetch('http://localhost:3001/')
+// 	// const response = fetch('http://127.0.0.1:3001/') <= how to get this to work?
+// 		.then(response => response.json())
+// 		.then(json => json)
+// 	console.log(await response)
+// }
+
+// async function fetchTableAwait() {
+// 	const response = await fetch('http://localhost:3001/')
+// 	// const response = await fetch('http://127.0.0.1:3001/') <= how to get this to work?
+// 	const json = await response.json()
+// 	console.log(json)
+// }
+
+const table = ref(null)
+const getTable = () => fetch('http://localhost:3001/')
+	.then(response => response.json())
+	.then(json => table.value = json)
+
+
+async function insertRow(data: string) {
+	const response = await fetch('http://localhost:3001/',{
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ data })
+	})
+	console.log((await response).json())
+	getTable()
 }
 
-async function fetchTableAwait() {
-	const response = await fetch('http://localhost:3001/')
-	// const response = await fetch('http://127.0.0.1:3001/') <= how to get this to work?
-	const json = await response.json()
-	console.log(json)
-}
+async function deleteLastRow(id: number) {
+	const response = await fetch('http://localhost:3001/',{
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ id })
+	})
+	console.log((await response).json())
+	getTable()
+	}
 
+	const newRow = ref('')
 </script>
 
 <template>
 	<div>
-		<button @click="fetchTableThen">Make Fetch Then Request</button>
-		<button @click="fetchTableAwait">Make Fetch AWAIT Request</button>
+		<button @click="getTable">Get Data</button><br>
+		<button @click="insertRow(newRow)">Create Row</button><input v-model="newRow"><br>
+		<button @click="deleteLastRow(table.data[table.data.length -1].id)">Delete Row</button>
+		<table>
+			<th>
+				<td>ID</td>
+				<td>Data</td>
+			</th>
+			<tr v-if="table" v-for="row in table.data">
+				<td>{{ row.id }}</td>
+				<td>{{ row.data }}</td>
+			</tr>
+		</table>
 	</div>
 </template>
 
