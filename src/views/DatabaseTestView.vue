201da35987aee65d133ba027d676b690631f4f88
Jakob Edelstein <jakobedelstein@gmail.com>
2024-03-29 20:30:30 -0500

    example of API call with .then() and await

---
+++ src/views/DatabaseTestView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+async function fetchTableThen() {
+	const response = fetch('http://localhost:3001/')
+		.then(response => response.json())
+		.then(json => json)
+
+	return response
+}
+console.log('fetchTableThen: ', await fetchTableThen())
+
+async function fetchTableAwait() {
+	const response = await fetch('http://localhost:3001/')
+	const json = await response.json()
+	return json
+}
+console.log('fetchTableAwait: ', await fetchTableAwait())
+
+</script>
+
+<template>
+	<div>
+		<button @click="fetchTableThen">Make Fetch Then Request</button>
+		<button @click="fetchTableAwait">Make Fetch AWAIT Request</button>
+	</div>
+</template>
+
+<style scoped>
+</style>
