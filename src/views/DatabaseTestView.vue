7a808c1585755c092833fadcfc35edb1b7b88a6f
Jakob Edelstein <jakobedelstein@gmail.com>
2024-03-29 20:57:29 -0500

    comment

--- src/views/DatabaseTestView.vue
+++ src/views/DatabaseTestView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 async function fetchTableThen() {
 	const response = fetch('http://localhost:3001/')
+	// const response = fetch('http://127.0.0.1:3001/') <= how to get this to work?
 		.then(response => response.json())
 		.then(json => json)
 	console.log(await response)
@@ -8,6 +9,7 @@ async function fetchTableThen() {
 
 async function fetchTableAwait() {
 	const response = await fetch('http://localhost:3001/')
+	// const response = await fetch('http://127.0.0.1:3001/') <= how to get this to work?
 	const json = await response.json()
 	console.log(json)
 }
