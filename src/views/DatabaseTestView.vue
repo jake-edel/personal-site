e5d318dcfcfa726351262bc98a68ba75a2d6ad8c
Jakob Edelstein <jakobedelstein@gmail.com>
2024-04-01 19:25:30 -0500

    function to update database row

--- src/views/DatabaseTestView.vue
+++ src/views/DatabaseTestView.vue
@@ -15,9 +15,10 @@ const lastElementId = computed(() => table?.data[table.data.length - 1].id)
 
 const getTable = () => fetch('http://localhost:3001/')
 	.then(response => response.json())
-	.then(json => table.data = json)
+	.then(json => table.data = json.data)
 
 const rowInput = ref()
+const newRowData = ref('')
 async function insertRow(data: string) {
 	if (data === '') return
 
@@ -29,7 +30,7 @@ async function insertRow(data: string) {
 		body: JSON.stringify({ data })
 	})
 	console.log(await response.json())
-	newRow.value = ''
+	newRowData.value = ''
 	rowInput.value.focus()
 	getTable()
 }
@@ -46,9 +47,24 @@ async function deleteLastRow(id: number | undefined) {
 	})
 	console.log(await response.json())
 	getTable()
-	}
+}
 
-const newRow = ref('')
+const editElementId = ref(0)
+const editElementValue = ref('')
+async function updateRow(id: number | string , data: string) {
+	if (id === '' || data === '') return
+
+	const response = await fetch('http://localhost:3001/',{
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ id, data })
+	})
+	console.log(await response.json())
+	getTable()
+}
+
 </script>
 
 <template>
@@ -57,14 +73,20 @@ const newRow = ref('')
 			Get Data
 		</button>
 		<br>
-		<button @click="insertRow(newRow)">
+		<button @click="insertRow(newRowData)">
 			Create Row
 		</button>
-		<input v-model="newRow" ref="rowInput">
+		<input v-model="newRowData" ref="rowInput">
 		<br>
 		<button @click="deleteLastRow(lastElementId)">
 			Delete Row
+		</button><br>
+		<button @click="updateRow(editElementId, editElementValue)">
+			Update Row
 		</button>
+		<input v-model="editElementId" type="number">
+		<input v-model="editElementValue">
+
 		<table>
 			<th>
 				<td>ID</td>
